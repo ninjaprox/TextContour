ba2a68865c69dc9b1520d9338e9f387cb8b4abb1
Vinh Nguyen <ninjaprox@gmail.com>
2017-02-28 18:05:23 +0800

    Upload script

---
+++ scripts/upload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FOLDER=ios-images
+
+objectName=$1
+file=$1
+bucket=$S3_BUCKET
+resource="/${bucket}/${FOLDER}/${objectName}"
+contentType=$2
+dateValue=$(date +"%a, %d %b %Y %T %z")
+stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+s3Key=$S3_ACCESS_ID
+s3Secret=$S3_ACCESS_SECRET
+signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+curl -v -i -X PUT -T "${file}" \
+          -H "Host: ${bucket}.s3.amazonaws.com" \
+          -H "Date: ${dateValue}" \
+          -H "Content-Type: ${contentType}" \
+          -H "Authorization: AWS ${s3Key}:${signature}" \
+          https://${bucket}.s3.amazonaws.com/${FOLDER}/${objectName}
