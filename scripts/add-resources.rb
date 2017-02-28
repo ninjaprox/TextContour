06b7776c60c4f2fa64511f650941fa786d889e3d
Vinh Nguyen <ninjaprox@gmail.com>
2017-02-28 12:21:31 +0800

    Add resources as folder references

--- scripts/add-resources.rb
+++ scripts/add-resources.rb
@@ -7,22 +7,14 @@ textContourGroup = project.groups.first
 
 # Add fonts
 puts 'Adding font resources...'
-fontGroup = textContourGroup.new_group('Fonts', 'Fonts')
-fontRefs = []
-Dir.new('TextContour/Fonts').each do |file|
-    fontRefs << fontGroup.new_reference(file) unless File.directory? file
-end
-target.add_resources(fontRefs)
+fontRef = textContourGroup.new_reference('Fonts')
+target.add_resources([fontRef])
 puts 'Adding font resources... Done.'
 
 # Add images
 puts 'Adding image resources...'
-imagesGroup = textContourGroup.new_group('Images', 'Images')
-imageRefs = []
-Dir.new('TextContour/Images').each do |file|
-    imageRefs << imagesGroup.new_reference(file) unless File.directory? file
-end
-target.add_resources(imageRefs)
+imageRef = textContourGroup.new_reference('Images')
+target.add_resources([imageRef])
 puts 'Adding image resources... Done.'
 
 project.save
