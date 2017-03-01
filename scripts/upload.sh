760370bdda94d51d94e30bd2623a6b35ed530908
Vinh Nguyen <ninjaprox@gmail.com>
2017-03-01 13:33:36 +0800

    Encode space in upload URL

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -3,6 +3,7 @@
 FOLDER=ios-images
 
 objectName=$1
+objectName=$(echo "$objectName" | sed "s/ /%20/g")
 file=$2
 bucket=$S3_BUCKET
 resource="/${bucket}/${FOLDER}/${objectName}"
@@ -12,9 +13,10 @@ stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
 s3Key=$S3_ACCESS_ID
 s3Secret=$S3_ACCESS_SECRET
 signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+url="https://${bucket}.s3.amazonaws.com/${FOLDER}/${objectName}"
 curl -v -i -X PUT -T "${file}" \
           -H "Host: ${bucket}.s3.amazonaws.com" \
           -H "Date: ${dateValue}" \
           -H "Content-Type: ${contentType}" \
           -H "Authorization: AWS ${s3Key}:${signature}" \
-          "https://${bucket}.s3.amazonaws.com/${FOLDER}/${objectName}"
+          "$url"
